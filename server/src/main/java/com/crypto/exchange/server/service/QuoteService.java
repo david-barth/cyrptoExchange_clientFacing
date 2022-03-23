9abb4e7f15f7686288c7dc7eab6f2a555ab057b3
david barth <dzb10035@yahoo.com>
2022-03-23 21:59:49 +0100

    CRYP-14-IMPL-1: Constructed first attempted at consuming external REST call with WebClient

--- server/src/main/java/com/crypto/exchange/server/service/QuoteService.java
+++ server/src/main/java/com/crypto/exchange/server/service/QuoteService.java
@@ -4,6 +4,9 @@ package com.crypto.exchange.server.service;
 import com.crypto.exchange.server.entity.Quote;
 import lombok.AllArgsConstructor;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.concurrent.atomic.AtomicReference;
 
 @AllArgsConstructor
 public class QuoteService {
@@ -12,10 +15,14 @@ public class QuoteService {
     private WebClient cryptoInfoClient;
 
     public Quote fetchQuoteInfo(String assetName) {
-        cryptoInfoClient.get()
+       Mono<Quote> testQuote = cryptoInfoClient.get()
                 .uri("/" + assetName)
                 .header("x-messari-api-key", "e067e272-c6da-4fc5-b381-ba448cc84187")
-                .retrieve();
+                .retrieve()
+                .bodyToMono(Quote.class);
+
+       AtomicReference<String> testName = new AtomicReference<>("");
+       testQuote.subscribe(quote -> testName.set(quote.getAssetName()));
 
         return new Quote();
     }
