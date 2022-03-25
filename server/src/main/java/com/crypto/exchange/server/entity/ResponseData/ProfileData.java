11ac0409e1fadde498a8bbeb5f73c6bd70714d7c
david barth <dzb10035@yahoo.com>
2022-03-25 21:29:44 +0100

    CRYP-14-IMPL-1: Added basic GET request for web client approach

---
+++ server/src/main/java/com/crypto/exchange/server/entity/ResponseData/ProfileData.java
@@ -0,0 +1,14 @@
+package com.crypto.exchange.server.entity.ResponseData;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+public class ProfileResponse {
+
+    
+    private String symbol;
+    private String name;
+    private String slug;
+}
--- server/src/main/java/com/crypto/exchange/server/service/QuoteService.java
+++ server/src/main/java/com/crypto/exchange/server/service/QuoteService.java
@@ -1,22 +1,2 @@
-package com.crypto.exchange.server.service;
-
-
-import com.crypto.exchange.server.entity.Quote;
-import lombok.AllArgsConstructor;
-import org.springframework.web.reactive.function.client.WebClient;
-
-
-
-@AllArgsConstructor
-public class QuoteService {
-
-    private WebClient cryptoInfoClient;
-
-    public Quote fetchQuoteInfo(String assetName) {
-        return cryptoInfoClient.get()
-                .uri(assetName)
-                .retrieve()
-                .bodyToMono(Quote.class) //Mono is a reactive programming based class that asynchronously returns a single instance of the object from the response body.
-                .block();  //Await like behavior needs to be implemented in order to ensure that the instance resolves down to the class that it is suposed to be mapped to.
-    }
+package com.crypto.exchange.server.service;public class QuoteService {
 }
